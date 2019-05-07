412c7c78c7e7ce9acd587980ef14333c5a60b067
richardchien7 <richardchien6@gmail.com>
2019-05-08 00:13:14 +0800

    2019/5/8

---
+++ app/src/main/java/com/example/ch1/recyclerview/LinearAdapter.java
@@ -0,0 +1,22 @@
+package com.example.ch1.recyclerview;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class LinearAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+}
---
+++ app/src/main/java/com/example/ch1/recyclerview/LinearRecyclerViewActivity.java
@@ -0,0 +1,20 @@
+package com.example.ch1.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.ch1.R;
+
+public class LinearRecyclerViewActivity extends AppCompatActivity {
+    private RecyclerView mRvMain;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_recycler_view);
+        mRvMain = (RecyclerView) findViewById(R.id.rv_main);
+        mRvMain.setLayoutManager(new LinearLayoutManager(LinearRecyclerViewActivity.this));
+        //mRvMain.setAdapter();
+    }
+}
