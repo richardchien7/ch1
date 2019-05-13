c588627fdf1a885cd20c7105134be8beccbd0cc5
richardchien7 <richardchien6@gmail.com>
2019-05-13 22:19:44 +0800

    Update LinearAdapter.java

--- app/src/main/java/com/example/ch1/recyclerview/LinearAdapter.java
+++ app/src/main/java/com/example/ch1/recyclerview/LinearAdapter.java
@@ -2,21 +2,29 @@ package com.example.ch1.recyclerview;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-public class LinearAdapter extends RecyclerView.Adapter {
+public class LinearAdapter extends RecyclerView.Adapter<LinearAdapter.LinearViewHolder> {
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    public LinearAdapter.LinearViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(LinearAdapter.LinearViewHolder viewHolder, int i) {
 
     }
 
     @Override
     public int getItemCount() {
         return 10;
+    }
+
+    class LinearViewHolder extends RecyclerView.ViewHolder{
+        public LinearViewHolder(View itemView){
+            super(itemView);
+        }
     }
 }
